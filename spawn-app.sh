e76b62b95810ae6f3c6faca37c0d37de9c0f2485
ssh24 <sshasan10@hotmail.com>
2017-06-20 13:28:31 -0400

    Fix app spawn script

--- spawn-app.sh
+++ spawn-app.sh
@@ -5,11 +5,11 @@ APPS="apps"
 REPO=$1
 ISSUE=$2
 
-# mkdir $APPS/$REPO/$REPO-$ISSUE
+mkdir $APPS/$REPO/$REPO-$ISSUE
 
 echo "Copying template over to make app: $REPO-$ISSUE ..." 
 
-# cp -r $TEMPLATES/$REPO-template/* $APPS/$REPO/$REPO-$ISSUE
+cp -r $TEMPLATES/$REPO-template/* $APPS/$REPO/$REPO-$ISSUE
 
 echo "Issue: https://github.com/strongloop/loopback-connector-$REPO/issues/$ISSUE" > $APPS/$REPO/$REPO-$ISSUE/README.md
 
