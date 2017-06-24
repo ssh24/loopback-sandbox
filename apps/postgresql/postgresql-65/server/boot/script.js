c7bfaae77ab2a61c5e095ed406c0f180b56a00af
ssh24 <sshasan10@hotmail.com>
2017-06-23 22:53:28 -0400

    Fix on postgresql issue#65

--- apps/postgresql/postgresql-65/server/boot/script.js
+++ apps/postgresql/postgresql-65/server/boot/script.js
@@ -5,7 +5,7 @@ var _ = require('lodash');
 module.exports = function(app) {
     var db = app.datasources.postgresqlDs;
 
-    db.discoverSchemas('book', {schema: 'public', relations: true}, function(err, result) {
+    db.discoverSchema('book', {schema: 'public', relations: true}, function(err, result) {
         if (err) throw err;
         console.log('\nDiscovered schemas for `Author` and `Book` table: ' + util.inspect(result, {depth:4}));
     });
