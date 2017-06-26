3ac362ff2ab6ba5bbb80693dd69640ecb772cdf3
ssh24 <sshasan10@hotmail.com>
2017-06-26 13:37:34 -0400

    Add mongodb inq test app

---
+++ apps/mongodb/mongodb-inq-app/server/boot/script.js
@@ -0,0 +1,35 @@
+'use strict';
+var util = require('util');
+var _ = require('lodash');
+
+module.exports = function(app) {
+  var db = app.datasources.mongoDs;
+  var Employee = app.models.Employee;
+
+  db.automigrate(function(err) {
+    if (err) throw err;
+    console.log('\nAutomigrate completed');
+
+    Employee.create([{
+      name: 'Foo',
+      age: 12,
+      reportsTo: ['Bob', 'Sam'],
+    }, {
+      name: 'Bar',
+      age: 24,
+      reportsTo: ['Sam'],
+    }, {
+      name: 'Baz',
+      age: 22,
+      reportsTo: ['John', 'Bob'],
+    }], function(err, result) {
+      if (err) throw err;
+      console.log('\nCreated instance: ' + util.inspect(result));
+
+      Employee.find({where: {reportsTo: {inq: ['Sam']}}}, function(err, result) {
+        if (err) throw err;
+        console.log('\nFound instance with inq: ' + util.inspect(result));
+      });
+    });
+  });
+};
