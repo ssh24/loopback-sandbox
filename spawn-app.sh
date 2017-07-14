61cd63a20e8100e180b8ff6c03637a852f365f51
ssh24 <sshasan10@hotmail.com>
2017-07-14 18:55:57 -0400

    Add support for juggler stories

--- spawn-app.sh
+++ spawn-app.sh
@@ -11,6 +11,10 @@ echo "Copying template over to make app: $REPO-$ISSUE ..."
 
 cp -r $TEMPLATES/$REPO-template/* $APPS/$REPO/$REPO-$ISSUE
 
-echo "Issue: https://github.com/strongloop/loopback-connector-$REPO/issues/$ISSUE" > $APPS/$REPO/$REPO-$ISSUE/README.md
+if [[ "$REPO" == *"juggler"* ]]; then
+	echo "Issue: https://github.com/strongloop/loopback-datasource-$REPO/issues/$ISSUE" > $APPS/$REPO/$REPO-$ISSUE/README.md
+else
+	echo "Issue: https://github.com/strongloop/loopback-connector-$REPO/issues/$ISSUE" > $APPS/$REPO/$REPO-$ISSUE/README.md
+fi
 
 echo "Copied app over to: $APPS/$REPO/$REPO-$ISSUE"
