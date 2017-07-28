50f333fcc8a231ac9751444856d059bdd9305e41
ssh24 <sshasan10@hotmail.com>
2017-07-28 16:11:46 -0400

    Add app on postgresql issue#282

---
+++ apps/postgresql/postgresql-282/server/boot/script.js
@@ -0,0 +1,18 @@
+'use strict';
+var util = require('util');
+var _ = require('lodash');
+
+module.exports = function(app) {
+    var db = app.datasources.postgresqlDs;
+    var City = app.models.City;
+
+    db.automigrate(function(err) {
+        if (err) throw err;
+        console.log('\nAutomigrate completed');
+
+        db.discoverModelProperties('city', function(err, props) {
+            if (err) throw err;
+            console.log('\nModel properties: ' + util.inspect(props));
+        });
+    });
+};
