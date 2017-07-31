4f600dd045e29c81c6f107ac13b8e444c2d00347
ssh24 <sshasan10@hotmail.com>
2017-07-31 10:56:20 -0400

    Fixes on postgresql app#282

--- apps/postgresql/postgresql-282/server/boot/script.js
+++ apps/postgresql/postgresql-282/server/boot/script.js
@@ -6,13 +6,18 @@ module.exports = function(app) {
     var db = app.datasources.postgresqlDs;
     var City = app.models.City;
 
-    db.automigrate(function(err) {
+    db.discoverSchemas('city', {schema: 'public'}, function(err, result) {
         if (err) throw err;
-        console.log('\nAutomigrate completed');
-
-        db.discoverModelProperties('city', function(err, props) {
-            if (err) throw err;
-            console.log('\nModel properties: ' + util.inspect(props));
-        });
+        console.log('\nModel definition: ' + util.inspect(result, {depth: 10}));
     });
+
+    // db.automigrate(function(err) {
+    //     if (err) throw err;
+    //     console.log('\nAutomigrate completed');
+
+    //     db.discoverModelProperties('city', function(err, props) {
+    //         if (err) throw err;
+    //         console.log('\nModel properties: ' + util.inspect(props));
+    //     });
+    // });
 };
